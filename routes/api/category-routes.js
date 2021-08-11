a3fe79f0abf4de8e503e02d7cae8d953ef22b4c1
Alice Waweru <80792502+alicewaweru28@users.noreply.github.com>
2021-08-11 13:23:52 -0700

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,91 +1,28 @@
-//TO DO: PUT Works (updates the DB), but I get a 400 in Insomnia. Error in console says can't read property 'filter' of undefined on line 97.
-
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Category, Product } = require('../../models');
 
-// The `/api/products` endpoint
+// The `/api/categories` endpoint
 
-// get a product by its 'id'
-router.get('/:id', async (req, res) => {
-    try {
-      const productData = await Product.findByPk(req.params.id, {
-        include: [
-          { model: Category},
-          { model: Tag, through: ProductTag, as: 'product_tags' },
-        ],
-      });
-  
-      if (!productData) {
-        res.status(404).json({ message: 'No product found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(productData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+});
 
-  // get a product by its 'id'
-router.get('/:id', async (req, res) => {
-    try {
-      const productData = await Product.findByPk(req.params.id, {
-        include: [
-          { model: Category},
-          { model: Tag, through: ProductTag, as: 'product_tags' },
-        ],
-      });
-  
-      if (!productData) {
-        res.status(404).json({ message: 'No product found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(productData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
 
-  // create new product
 router.post('/', (req, res) => {
-    Product.create(req.body)
-      .then((product) => {
-        // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-        if (req.body.tagIds && req.body.tagIds.length) {
-          const productTagIdArr = req.body.tagIds.map((tag_id) => {
-            return {
-              product_id: product.id,
-              tag_id,
-            };
-          });
-          return ProductTag.bulkCreate(productTagIdArr);
-        }
+  // create a new category
+});
 
-        // get all products
-router.get('/', async (req, res) => {
-    try {
-      const productData = await Product.findAll({
-        include: [
-          { 
-            model: Category,
-            attributes: ['category_name']
-          },
-          { 
-            model: Tag, through: ProductTag, as: 'product_tags',
-            attributes: ['tag_name']
-          },
-        ],
-      });
-      if(!productData) {
-        res.status(404).json({ message: 'No products were found!'});
-        return;
-      }
-      res.status(200).json(productData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
